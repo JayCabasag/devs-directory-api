3cd30ad6af6fbce01cade78e5b22a8064aaf5103
Jay Cabasag <jaycabasag1999@gmail.com>
2023-06-13 03:09:20 +0800

    Reverted

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,23 +1,8 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.provider';
-import * as mongoose from 'mongoose';
 
 @Module({
     providers: [...databaseProviders],
     exports: [...databaseProviders],
 })
-export class DatabaseModule {
-    async onModuleInit() {
-        await this.connectToDatabase();
-    }
-
-    private async connectToDatabase() {
-        try {
-            await mongoose.connect(process.env.MONGO_URI);
-            console.log('MongoDB connected');
-        } catch (error) {
-            console.log(error);
-            process.exit(1);
-        }
-    }
-}
+export class DatabaseModule { }
