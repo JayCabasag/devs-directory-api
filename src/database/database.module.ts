d6dab6a5296bc851fa9544acb3c25885fe8816a9
Jay Cabasag <jaycabasag1999@gmail.com>
2023-06-13 03:06:57 +0800

    Added initial connections

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.provider';
+import * as mongoose from 'mongoose';
 
 @Module({
     providers: [...databaseProviders],
     exports: [...databaseProviders],
 })
-export class DatabaseModule { }
+export class DatabaseModule {
+    async onModuleInit() {
+        await this.connectToDatabase();
+    }
+
+    private async connectToDatabase() {
+        try {
+            await mongoose.connect(process.env.MONGO_URI);
+            console.log('MongoDB connected');
+        } catch (error) {
+            console.log(error);
+            process.exit(1);
+        }
+    }
+}
